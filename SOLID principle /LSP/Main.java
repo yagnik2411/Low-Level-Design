84081a16e12b3451dc773e945d6ae73382a19e9e
yagnik2411 <panchalyagnik2411@gmail.com>
2025-12-23 17:52:54 +0530

    Liskov Substitution  Principle

---
+++ SOLID principle /LSP/Main.java
@@ -0,0 +1,30 @@
+// This is example of Liskov Substitution Principle which S from Solid principle
+class Notification {
+    public void sendNotification(){
+        System.out.println("Email Notification");
+    }
+}
+
+class TextNotification extends Notification{
+
+    @Override
+    public void sendNotification(){
+        System.out.println("Text Notificatin");
+    }
+}
+
+class WpNotification extends Notification{
+
+    @Override
+    public void sendNotification(){
+        System.out.println("Wp Notificatin");
+    }
+}
+
+
+public class Main {
+    public static void main(String[] args) {
+        Notification notification = new TextNotification();
+        notification.sendNotification();
+    }
+}
